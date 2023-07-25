303c3c9307ea0de268619583083c5df7274fbf41
heberGustavo <heber.gbarbosa@hotmail.com>
2023-07-25 11:41:15 -0300

    Add project in repository

---
+++ MVC/Teste-GCASPP/Teste.Domain/Business/FilhoBusiness.cs
@@ -0,0 +1,64 @@
+﻿using Teste.Domain.Business.Base;
+using Teste.Domain.IBusiness;
+using Teste.Domain.IRepository;
+using Teste.Domain.Models.EntityDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Teste.Domain.Models.Body;
+
+namespace Teste.Domain.Business
+{
+    public class FilhoBusiness : BusinessBase<Filho>, IFilhoBusiness
+    {
+        private readonly IFilhoRepository _filhoRepository;
+
+        public FilhoBusiness(IFilhoRepository filhoRepository) : base(filhoRepository)
+        {
+            _filhoRepository = filhoRepository;
+        }
+
+        public async Task<ResultResponseModel> Cadastrar(Filho model)
+        {
+            try
+            {
+                var result = await _filhoRepository.CreateAsync(model);
+                if (result == 0) return new ResultResponseModel(true, "Erro ao cadastrar Filho. Tente novamente!");
+
+                return new ResultResponseModel(false, "Filho cadastrado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return new ResultResponseModel(true, "Erro ao cadastrar Filho. Contate o administrador.");
+            }
+        }
+
+        public async Task<ResultResponseModel> Editar(Filho model)
+        {
+            try
+            {
+                var result = await _filhoRepository.UpdateAsync(model);
+                if (result == null) return new ResultResponseModel(true, "Erro ao editar Filho. Tente novamente.");
+
+                return new ResultResponseModel(false, "Filho alterado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return new ResultResponseModel(true, "Ocorreu um erro. Contate o administrador.");
+            }
+        }
+
+        public Task<ResultResponseModel> Excluir(int id)
+            => _filhoRepository.Excluir(id);
+
+        public Task<Filho> ObterFilhoPorId(int id)
+            => _filhoRepository.GetById(id);
+
+        public Task<IEnumerable<FilhoBody>> ObterTodosFilhos()
+            => _filhoRepository.ObterTodosFilhos();
+
+        public Task<int> VerificaSeExisteFilhoCadastrado(int id)
+            => _filhoRepository.VerificaSeExisteFilhoCadastrado(id);
+    }
+}
---
+++ MVC/Teste-GCASPP/Teste/wwwroot/js/home/index.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    ListarDadosFuncionario();
+});
+
+function ListarDadosFuncionario() {
+
+    $.ajax({
+        url: "/Home/ListarDadosFuncionario",
+        type: "GET",
+        contentType: 'application/json; charset=UTF-8',
+        success: function (response) {
+            $("#divListarFuncionarios").html(response)
+        },
+        error: function (response) {
+            console.log(response)
+            swal("Opss", "Aconteceu um imprevisto na listagem dos Funcionarios. Contate o administrador!", "error");
+        }
+    });
+
+}
