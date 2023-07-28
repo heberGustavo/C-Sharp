23bd89aa5a35d31932a15118598fc9582a95e522
heberGustavo <heber.gbarbosa@hotmail.com>
2023-07-28 07:44:14 -0300

    Startup Configuration. Creating entity domain "Usuario" and classes in "Utils"

--- MVC/CalculadoraPRO/CalculadoraPRO/Startup.cs
+++ MVC/CalculadoraPRO/CalculadoraPRO/Startup.cs
@@ -1,34 +1,105 @@
+using AutoMapper;
+using CalculadoraPRO.CrossCutting.DependencyGroups;
+using CalculadoraPRO.CrossCutting.MappingGroups;
+using CalculadoraPRO.Domain.IBusiness.Migration;
+using CalculadoraPRO.Portal.Utils;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CalculadoraPRO
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
+        public IWebHostEnvironment Env { get; set; }
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            #region Cookie Policy
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+                options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+            });
+
+            #endregion
+
+            #region Mapper Configuration
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainToData>();
+                cfg.AddProfile<DataToDomain>();
+            });
+
+            IMapper mapper = configuration.CreateMapper();
+            services.AddSingleton(mapper);
+
+            #endregion
+
+            #region Dependency Injection
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            DataDependencyInjection.Register(services);
+            DomainDependencyInjection.Register(services);
+
+            #endregion
+
+            #region Authentication
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+               {
+                   options.Cookie.HttpOnly = true;
+                   options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                   options.Cookie.SameSite = SameSiteMode.None;
+                   options.Cookie.Name = ".AdministrativoImperial.AuthCookie";
+                   options.LoginPath = "/Login/Index";
+                   options.LogoutPath = "/Logout";
+                   options.Cookie.MaxAge = TimeSpan.FromDays(1);
+               });
+
+            #endregion
+
+            PolicyKeys.ConfigurePolicies(services);
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.AddControllersWithViews(options => options.Filters.Add(typeof(ValidateModelState)));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, [FromServices] IMigrationBusiness migrationBusiness)
         {
+            #region Definindo a cultura padrão: pt-BR
+            var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+            #endregion
+
+            migrationBusiness.ExecutarAtualizacaoBancoDados();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,21 +107,18 @@ namespace CalculadoraPRO
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=FuncaoFuncionario}/{action=Index}/{id?}");
             });
         }
     }
