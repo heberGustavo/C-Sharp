f285a3979c9dfcb10f06e57f639cf495d56eb1ca
heberGustavo <heber.gbarbosa@hotmail.com>
2023-10-11 09:30:01 -0300

    #35 - Ajuste no layout da tela de Dias Trabalhados

---
+++ MVC/Administrativo-Imperial/AdministrativoImperial.Domain/Models/EntityDomain/DiasTrabalhadosDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace AdministrativoImperial.Domain.Models.EntityDomain
+{
+    public class DiasTrabalhadosDTO
+    {
+        [DataMember]
+        public int Dit { get; set; }
+
+        [DataMember]
+        [Required(ErrorMessage = "O campo Funcionário é obrigatório")]
+        public int FunId { get; set; }
+
+        [DataMember]
+        [Required(ErrorMessage = "O campo Obra é obrigatório")]
+        public int ObrId { get; set; }
+
+    }
+}
---
+++ MVC/Administrativo-Imperial/AdministrativoImperial/wwwroot/js/Pages/DiasTrabalhados/salvar.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+
+});
+
+function ObterDadosTelaJsonCadastrar() {
+
+    var orcamentoFloat;
+
+    if (!IsNullOrEmpty(orcamento.val()))
+        orcamentoFloat = ConverterParaFloat(orcamento.val());
+    else
+        orcamentoFloat = 0.0;
+
+    return {
+        ObrDataInicio: dataInicio.val(),
+        ObrApelido: apelido.val(),
+        ObrEndereco: endereco.val(),
+        ObrOrcamento: orcamentoFloat,
+        ObrId: txtIdObraTemp.val().length <= 0 ? 0 : parseInt(txtIdObraTemp.val())
+    }
+}
+
+/*MODAL*/
+function ModalObraSalvar() {
+    LimparCamposModal();
+
+    if (VerificarCamposObrigatorios()) {
+        var json = ObterDadosTelaJsonCadastrar();
+
+        $.ajax({
+            url: "/Obra/Cadastrar",
+            type: "POST",
+            contentType: 'application/json; charset=UTF-8',
+            dataType: "json",
+            data: JSON.stringify(json),
+            success: function (response) {
+                if (!response.erro) {
+                    swal("Sucesso", response.mensagem[0], "success").then((confirm) => {
+                        if (confirm) {
+                            BuscarListaObras();
+                            LimparCamposModal();
+                            AlterarVisibilidadeAtualModal('modalObra');
+                        }
+                    });
+                }
+                else {
+                    $.each(response.mensagem, function (index, value) {
+                        MostrarAlertMensagemErro(value)
+                    });
+                }
+            },
+            error: function (response) {
+                console.log(response);
+                swal("Erro", "Aconteceu um imprevisto. Contate o administrador", "error");
+            }
+        });
+
+    }
+}
+
+function ModalObraEditar(obrId) {
+    LimparCamposModal();
+
+    txtIdObraTemp.val(obrId);
+
+    $.ajax({
+        url: "Obra/Selecionar/" + obrId,
+        type: "GET",
+        contentType: 'application/json; charset=UTF-8',
+        dataType: "json",
+        success: function (response) {
+            if (!response.erro) {
+                PreencherModalObra(response);
+                AlterarVisibilidadeAtualModal('modalObra');
+            }
+            else {
+                $.each(response.mensagem, function (index, value) {
+                    MostrarAlertMensagemErro(value)
+                });
+            }
+        },
+        error: function (response) {
+            console.log(response);
+            swal("Erro", "Aconteceu um imprevisto. Contate o administrador", "error");
+        }
+    });
+}
