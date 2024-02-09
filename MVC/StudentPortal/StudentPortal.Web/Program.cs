408229b40ea850beaf51cb206e77a5e67bf4bfe9
heberGustavo <heber.gbarbosa@hotmail.com>
2024-02-09 08:53:48 -0300

    Creating View and Methods List, Add and Edit to Students. Creating database and DbContext

--- MVC/StudentPortal/StudentPortal.Web/Program.cs
+++ MVC/StudentPortal/StudentPortal.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
 
 var app = builder.Build();
 
@@ -22,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Students}/{action=List}/{id?}");
 
 app.Run();
