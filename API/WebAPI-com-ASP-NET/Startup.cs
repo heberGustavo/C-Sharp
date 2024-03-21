d1e2244582f2b1ec7a1de665a5d41c663dbe8a19
heberGustavo <heber.gbarbosa@hotmail.com>
2024-03-21 07:27:35 -0300

    Adding Swagger

--- API/WebAPI-com-ASP-NET/Startup.cs
+++ API/WebAPI-com-ASP-NET/Startup.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SmartSchool_WEBAPI.Data;
+using System;
 
 namespace SmartSchool_WEBAPI
 {
@@ -20,7 +22,7 @@ namespace SmartSchool_WEBAPI
         public void ConfigureServices(IServiceCollection services)
         {
             //Conexão com o banco de dados
-            var connection = Configuration["ConexaoSqlite:SqliteConnectionString"];
+			var connection = Configuration["ConexaoSqlite:SqliteConnectionString"];
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlite(connection)
             );
@@ -30,19 +32,42 @@ namespace SmartSchool_WEBAPI
 
             //Ingeção de dependencias 
             services.AddScoped<IRepository, Repository>();
-        }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+			services.AddSwaggerGen();
+
+			services.AddSwaggerGen();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Version = "v1",
+					Title = "API SmartSchool",
+					Description = "The project was developed using .NET Web API, Entity Framework Core and SQ Lite",
+					Contact = new OpenApiContact
+					{
+						Name = "Heber Gustavo",
+						Url = new Uri("https://www.linkedin.com/in/heber-gustavo/")
+					}
+				});
+			});
+
+
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
+			app.UseSwagger();
+			app.UseSwaggerUI(c => {
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gerenciamento de Produtos API");
+			});
 
+			app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseAuthorization();
 
             //Liberar acesso do CORS
